b591242d1b51fe38cd8981167f4f261ddb7f053d
zzase <sehoon9867@naver.com>
2021-09-08 17:17:44 +0900

    PeterPetDid.sol : delete getPetInfo*()

--- backend/contracts/PeterPetDid.sol
+++ backend/contracts/PeterPetDid.sol
@@ -7,10 +7,10 @@ import "./String.sol";
 contract PeterPetDID is Ownable, ToString {
     struct PeterPet {
         string name;
-        string birth;
         string breedOfDog;
         string gender;
-        string adoptionDate;
+        uint birth;
+        uint adoptionDate;
         bool isNeutering;
         string furColor;
         string vaccinationHistory;
@@ -21,8 +21,8 @@ contract PeterPetDID is Ownable, ToString {
         string did;
     }
 
-    PeterPet[] peterPets;
-    Did[] dids;
+    PeterPet[] public peterPets;
+    Did[] public dids;
 
     uint index;
     string didPre = "did:peterpet:";
@@ -30,13 +30,15 @@ contract PeterPetDID is Ownable, ToString {
     mapping(string => PeterPet) didToPetMapper;
     mapping(address => PeterPet[]) addressToPetMapper;
 
-    
+    constructor() public {
+        addressToPetMapper[msg.sender] = peterPets;
+    }
 
-    function makeDid(uint _index) public view returns (string memory) {
+    function makeDid(uint _index) private view returns (string memory) {
         return string(abi.encodePacked(didPre,toString(keccak256(abi.encodePacked(msg.sender,_index))))) ;
     }
 
-    function addDid(string memory _name, string memory _birth, string memory _breedOfDog, string memory _gender, string memory _adoptionDate, 
+    function addDid(string memory _name, uint _birth, string memory _breedOfDog, string memory _gender, uint _adoptionDate, 
     bool _isNeutering, string memory _furColor, string memory _vaccinationHistory, string memory _notes) internal onlyOwner {
         addPet(_name, _birth, _breedOfDog, _gender, _adoptionDate, _isNeutering, _furColor, _vaccinationHistory, _notes);
         index = peterPets.length - 1;
@@ -46,44 +48,19 @@ contract PeterPetDID is Ownable, ToString {
         }
     } 
 
-    function addPet(string memory _name, string memory _birth, string memory _breedOfDog, string memory _gender, string memory _adoptionDate, 
-    bool _isNeutering, string memory _furColor, string memory _vaccinationHistory, string memory _notes) public {
-        peterPets.push(PeterPet(_name,_birth,_breedOfDog, _gender, _adoptionDate, _isNeutering, _furColor, _vaccinationHistory, _notes));
+    function addPet(string memory _name, uint _birth, string memory _breedOfDog, string memory _gender, uint _adoptionDate, 
+    bool _isNeutering, string memory _furColor, string memory _vaccinationHistory, string memory _notes) private {
+        peterPets.push(PeterPet(_name,_breedOfDog, _gender,_birth, _adoptionDate, _isNeutering, _furColor, _vaccinationHistory, _notes));
     }
 
     function updateDid() public {
 
     }
 
-    function getPetInfoByDid(string memory _did) public view returns(string memory _name, string memory _birth, string memory _breedOfDog, string memory _gender, string memory _adoptionDate, 
-    bool _isNeutering, string memory _furColor, string memory _vaccinationHistory, string memory _notes) {
-        _name = didToPetMapper[_did].name;
-        _birth = didToPetMapper[_did].birth;
-        _breedOfDog = didToPetMapper[_did].breedOfDog;
-        _gender = didToPetMapper[_did].gender;
-        _adoptionDate = didToPetMapper[_did].adoptionDate;
-        _isNeutering = didToPetMapper[_did].isNeutering;
-        _furColor = didToPetMapper[_did].furColor;
-        _vaccinationHistory = didToPetMapper[_did].vaccinationHistory;
-        _notes = didToPetMapper[_did].notes;
-    }
-
-    // function getPetInfoByAddress(address _owner, uint _index) public view returns (string memory _name, string memory _birth, string memory _breedOfDog, string memory _gender, string memory _adoptionDate, 
-    // bool _isNeutering, string memory _furColor, string memory _vaccinationHistory, string memory _notes) {
-        
-    //     _name = addressToPetMapper[_owner][_index].name;
-    //     _birth = addressToPetMapper[_owner][_index].birth;
-    //     _breedOfDog = addressToPetMapper[_owner][_index].breedOfDog;
-    //     _gender = addressToPetMapper[_owner][_index].gender;
-    //     _adoptionDate = addressToPetMapper[_owner][_index].adoptionDate;
-    //     _isNeutering = addressToPetMapper[_owner][_index].isNeutering;
-    //     _furColor = addressToPetMapper[_owner][_index].furColor;
-    //     _vaccinationHistory = addressToPetMapper[_owner][_index].vaccinationHistory;
-    //     _notes = addressToPetMapper[_owner][_index].notes;
-    // }
-
-    function checkLength(uint _peterPetsLength, uint _didsLength) public pure returns (bool) {
+    function checkLength(uint _peterPetsLength, uint _didsLength) private pure returns (bool) {
         if(_peterPetsLength == _didsLength) return true;
         else return false;
     }
+
+    
 }
