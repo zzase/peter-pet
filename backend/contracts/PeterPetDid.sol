e8fa136a282d8cc7a2d80236d12d2e941de974b5
zzase <sehoon9867@naver.com>
2021-09-09 11:27:08 +0900

    PeterPetDid.sol : 각 변수 및 함수 주석 설명 추가

--- backend/contracts/PeterPetDid.sol
+++ backend/contracts/PeterPetDid.sol
@@ -6,19 +6,19 @@ import "./String.sol";
 
 contract PeterPetDID is Ownable, StringFormat {
     struct PeterPet {
-        string name;
-        string breedOfDog;
-        string gender;
-        uint birth;
-        uint adoptionDate;
-        bool isNeutering;
-        string furColor;
-        string vaccinationHistory;
-        string notes;
+        string name; // 이름
+        string breedOfDog; // 견종
+        string gender; //성별
+        uint birth; //생년월일
+        uint adoptionDate; // 입양일
+        bool isNeutering; // 중성화 여부
+        string furColor; //모색
+        string vaccinationHistory; //특이사항
+        string notes; //접종내역
     }
 
     struct Did {
-        string did;
+        string did; // did 번호
     }
 
     PeterPet[] peterPets;
@@ -34,10 +34,27 @@ contract PeterPetDID is Ownable, StringFormat {
         addressToPetMapper[msg.sender] = peterPets;
     }
 
+    /*
+     * makeDid() 
+     * - did 만드는 함수, did = did:peterpet: (계정주소 + 펫 인덱스 + 난수)의 해시값 
+     */
     function makeDid(uint _index) private view returns (string memory) {
         return string(abi.encodePacked(didPre,toString(keccak256(abi.encodePacked(msg.sender,_index))))) ;
     }
 
+    /*
+     * addPet() 
+     * - 입력받은 반려견 정보를 peterPets 배열에 push 
+     */
+    function addPet(string memory _name, uint _birth, string memory _breedOfDog, string memory _gender, uint _adoptionDate, 
+    bool _isNeutering, string memory _furColor, string memory _vaccinationHistory, string memory _notes) private {
+        peterPets.push(PeterPet(_name,_breedOfDog, _gender,_birth, _adoptionDate, _isNeutering, _furColor, _vaccinationHistory, _notes));
+    }
+
+    /*
+     * addDid() 
+     * - addPet()과 makeDid()를 호출하여 반려견 정보를 담은 did를 만들고 did와 반려견정보를 매핑
+     */
     function addDid(string memory _name, uint _birth, string memory _breedOfDog, string memory _gender, uint _adoptionDate, 
     bool _isNeutering, string memory _furColor, string memory _vaccinationHistory, string memory _notes) public onlyOwner {
         addPet(_name, _birth, _breedOfDog, _gender, _adoptionDate, _isNeutering, _furColor, _vaccinationHistory, _notes);
@@ -48,11 +65,10 @@ contract PeterPetDID is Ownable, StringFormat {
         }
     } 
 
-    function addPet(string memory _name, uint _birth, string memory _breedOfDog, string memory _gender, uint _adoptionDate, 
-    bool _isNeutering, string memory _furColor, string memory _vaccinationHistory, string memory _notes) private {
-        peterPets.push(PeterPet(_name,_breedOfDog, _gender,_birth, _adoptionDate, _isNeutering, _furColor, _vaccinationHistory, _notes));
-    }
-
+    /*
+     * updateDid() 
+     * - did를 통해 기존 반려견 정보 수정  
+     */
     function updateDid(string memory _did, string memory _name, uint _birth, string memory _breedOfDog, string memory _gender, uint _adoptionDate, 
     bool _isNeutering, string memory _furColor, string memory _vaccinationHistory, string memory _notes ) public {
         didToPetMapper[_did].name = _name;
@@ -65,6 +81,11 @@ contract PeterPetDID is Ownable, StringFormat {
         didToPetMapper[_did].vaccinationHistory = _vaccinationHistory;
         didToPetMapper[_did].notes = _notes;
     }
+
+    /*
+     * getPetInfoByDid() 
+     * - pet 정보를 did를 통해 가져옴 
+     */
      function getPetNameByDid(string memory _did) public view returns(string memory _name) {
         _name = didToPetMapper[_did].name; 
      }
@@ -101,6 +122,10 @@ contract PeterPetDID is Ownable, StringFormat {
         _notes = didToPetMapper[_did].notes; 
      }
      
+     /*
+     * checkLength() 
+     * - 등록된 pet과 did의 수가 일치하는지 확인
+     */
     function checkLength(uint _peterPetsLength, uint _didsLength) private pure returns (bool) {
         if(_peterPetsLength == _didsLength) return true;
         else return false;
