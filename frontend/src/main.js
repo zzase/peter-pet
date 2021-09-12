fa9f04ac4ff9452f9d88bc11160a5617e3d33718
zzase <sehoon9867@naver.com>
2021-09-12 16:47:03 +0900

    install and setup axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -18,6 +18,7 @@ import router from "./router";
 import VueComp from '@vue/composition-api';
 import MaterialKit from "./plugins/material-kit";
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import axios from 'axios'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
@@ -27,6 +28,8 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false;
 Vue.use(MaterialKit);
