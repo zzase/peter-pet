8a33db2b18802fd85bfa95676a4b486ea621c127
zzase <sehoon9867@naver.com>
2021-09-12 16:57:09 +0900

    make result page

---
+++ frontend/src/views/result.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p>{{peterpet.name}}</p>
+        <p>{{peterpet.gender}}</p>
+        <p>{{peterpet.birth}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            peterpet: null,
+        };
+    },
+    created() {
+        this.$http.get('/api/pet')
+        .then((res) => {
+            const peterpet = res.data.peterpet;
+            if(peterpet) this.peterpet = peterpet;
+        })
+        .catch((err)=> {
+            console.error(err);
+        });
+    }
+}
+</script>
