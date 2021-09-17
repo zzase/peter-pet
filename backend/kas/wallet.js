775689f672d217b6412de1c7bcafde917f18637c
zzase <sehoon9867@naver.com>
2021-09-17 23:18:56 +0900

    make apiCaller, wallet

---
+++ backend/kas/wallet.js
@@ -0,0 +1,21 @@
+import ApiCaller from "./apiCaller";
+
+class Wallet extends ApiCaller {
+    constructor() {
+        super('https://wallet-api.klaytnapi.com');
+    }
+
+    async createAccount() {
+        const options = {
+            method: 'POST',
+            url: '/v2/account',
+            json: true,
+        };
+
+        return await this.call(options);
+    }
+}
+
+const wallet = new Wallet();
+
+module.exports = wallet;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/create',async function(req,res,next) {
+
+})
+
 module.exports = router;
+ 
