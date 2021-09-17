4818ea98ad63e0002120173e176c29e79b308c02
zzase <sehoon9867@naver.com>
2021-09-17 17:40:16 +0900

    app.js : update mySql connection

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password:'hoon3827',
+  database: 'peterpet'
+});
+
+connection.connect(function (err){
+  if(err) {
+    console.error('mysql connection error');
+    console.error(err);
+    throw err;
+  }
+  else console.log("success mysql connect");
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
