4dcb30337b242e9c406cc24ea27f358a8edf3032
ether <ether@peer0.example.com>
2021-09-22 01:43:59 +0900

    mypage changed

--- frontend/src/views/MyPage.vue
+++ frontend/src/views/MyPage.vue
@@ -10,6 +10,32 @@
                 </div>
                   <div>
                   <Side></Side>
+                  <div class="welcome">
+                  <h4>00님<br><br>환영합니다</h4>
+                  </div>
+                    <div class="my-account">
+                    </div>
+                      <div class="line">
+                      </div>
+                        <div class="accountbox1">
+                        </div>
+                        <div class="accountbox2">
+                        </div>
+                        <div class="sample">
+                        </div>
+                        <div class="account-number">
+                          <h6>내 계좌</h6>
+                        </div>
+                        <div class="klay">
+                          <h6>나의 잔액</h6>
+                        </div>
+                        <div class="accountNumber">
+                        <p style="color: darkgray"> accountNumber {{ accountNumber }} </p>
+                      </div>
+                      <div class="myKlay">
+                        <p style="color: darkgray"> myKlay {{ myKlay }} </p>
+                      </div>
+                       
                   </div>
                   <!-- <div class="text">
                   <h4>My Account</h4>
@@ -86,23 +112,87 @@ export default {
   margin-top: 50px;
   left: -5%;
 }
-.account {
-  position: relative;
-  width: 1120px;
-  height: 200px;
-  left: -5%;
-  border: 2px solid rgba(223, 223, 223, 0.849);
-  border-radius: 2em;
-}
 .registercard{
   position: relative;
   top: 10%;
   width: 40%;
   height:40%;
   left: -6%;
+}
+.my-account {
+  display: inline-block;
+  position: absolute;
+  bottom: 44%;
+  width: 25%;
+  height: 13%;
+  border: 2px solid rgba(240, 240, 240, 0.877);
+  left: 28%;
+  border-radius: 2em;
   
 }
+.accountbox1 {
+  z-index: 1;
+  position:absolute;
+  display: inline-block;
+  width: 25%;
+  height: 13%;
+  left: 0%;
+  bottom: 44%;
+  border:2px solid rgba(240, 240, 240, 0.877);
+  border-radius: 2em;
+  
+  
+}
+.line {
+  position: absolute;
+  display: inline-block;
+  width: 55%;
+  height: 0.5%;
+  background-color: rgba(240, 240, 240, 0.377);
+  left: -1%;
+  bottom: 60%;
+}
 
-
-
+.welcome {
+  position: absolute;
+  display: inline-block;
+  bottom: 63%;
+  left: 0.2%;
+}
+.sample {
+  position:absolute;
+  display: inline-block;
+  width: 55%;
+  height: 20%;
+  background-color: rgba(240, 240, 240, 0.377);
+  bottom: 18%;
+  left: -1%;
+}
+.account-number {
+  position: absolute;
+  display: inline-block;
+  bottom: 52%;
+  left: 2%;
+}
+.klay {
+  position: absolute;
+  display: inline-block;
+  bottom: 52%;
+  left: 30%;
+}
+p {
+  font-size: 20px;
+}
+.accountNumber {
+  position: absolute;
+  display: inline-block;
+  bottom: 46%;
+  left: 2%;
+}
+.myKlay {
+  position: absolute;
+  display: inline-block;
+  bottom: 46%;
+  left: 30%;
+}
 </style>
