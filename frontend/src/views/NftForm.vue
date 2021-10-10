55c1544695edb29d7e932b7f454b1b8936c96c5a
ether <ether@peer0.example.com>
2021-10-10 21:03:14 +0900

    Peterpet Mall item list added

--- frontend/src/views/NftForm.vue
+++ frontend/src/views/NftForm.vue
@@ -60,9 +60,8 @@
             </div>
 
             <br />
-
+          <label class="choose-d"> 동물등록증을 선택해주세요* </label><br />
             <div class="choose-did">
-              <label class="choose-d"> 동물등록증을 선택해주세요* </label><br />
               <b-form-select v-model="metadata.did" :options="options">
                 <template #first>
                   <b-form-select-option :value="null" disabled
@@ -104,6 +103,9 @@
             </div>
             <!-- 혈통증명서 -->
 
+           <label class="cert">
+                혈통증명서 첨부
+              </label>
             <div class="certification">
               <b-form-file multiple="multiple">
                 <template slot="file-name" slot-scope="{ names }">
@@ -199,12 +201,15 @@ export default {
 
 <style>
 @media screen and (min-width:1280px) {
+.custom-file-input{
+  margin-left: -450px
+}
 
 .custom-select {
   width: 100%;
 }
 .choose-d {
-    float: left;
+  margin-left: -730px;
   }
 .choose-did {
   width: 60%;
@@ -251,6 +256,7 @@ export default {
   font-size: 1.8rem;
 }
 .create-box {
+  margin-top: 50px;
   width: 100%;
   max-width: 800px;
   margin: 0 auto;
